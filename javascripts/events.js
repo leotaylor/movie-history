d7c274c284d86be055fb259968469b8f86267fbc
Leo Taylor <leotaylor723@gmail.com>
2018-06-05 19:28:45 -0500

    error message

--- javascripts/events.js
+++ javascripts/events.js
@@ -136,16 +136,18 @@ const authEvents = () => {
     const pass = $('#inputPassword').val();
     firebase.auth().signInWithEmailAndPassword(email, pass)
       .catch((error) => {
-        const errorMessage = error.message;
-        console.error(errorMessage);
+        $('#signin-error-msg').text(error.message);
+        $('#signin-error').removeClass('hide');
+        console.error(error.message);
       });
   });
   $('#register-btn').click(() => {
     const email = $('#registerEmail').val();
     const pass = $('#registerPassword').val();
     firebase.auth().createUserWithEmailAndPassword(email, pass).catch((error) => {
-      const errorMessage = error.message;
-      console.error(errorMessage);
+      $('#register-error-msg').text(error.message);
+      $('#register-error').removeClass('hide');
+      console.error(error.message);
     });
   });
 
